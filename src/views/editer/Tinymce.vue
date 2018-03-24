718cedce306945f1ef4fbe3ae4b1473ed7e71812
artiely <1119696785@qq.com>
2018-03-24 12:50:39 +0800

    整理tinymce

--- src/views/editer/Tinymce.vue
+++ src/views/editer/Tinymce.vue
@@ -1,20 +1,25 @@
 <template>
+<div>
   <tinymce id="d1" v-model="data" :other_options="options"></tinymce>
+  <pre>{{data}}</pre>
+</div>
 </template>
 <script>
+let lang = 'zh_CN'
 export default {
   data() {
     return {
       data: '欢迎使用 tinymce',
+      lang: 0,
       options: {
-        language_url: require('./langs/zh_CN')
+        language_url: lang === 'zh_CN' ? require('./langs/zh_CN') : require('./langs/en_CA')
       }
     }
   }
 }
 </script>
 <style>
-.mce-title{
+.mce-title {
   visibility: hidden;
 }
 </style>
